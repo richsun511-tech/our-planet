b2936ac244a13457031c7ee7e906b09ce979b887
richsun511-tech <richsun511@gmail.com>
2026-06-09 13:41:30 +0800

    SW: force update every load

--- sw.js
+++ sw.js
@@ -1,12 +1,3 @@
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-});
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
-    }).then(function() {
-      return self.clients.claim();
-    })
-  );
-});
+self.addEventListener('install',function(e){self.skipWaiting()});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.map(function(n){return caches.delete(n)}))}).then(function(){return self.clients.claim()}))});
+self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request))});
